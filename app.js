e069656a2b4af7d3a6bceb842e6e6d1894b706ed
Sethu_Meena <sethumeenakaruppiah@gmail.com>
2020-10-30 14:59:53 -0400

    Added touch event for mobile touch

--- app.js
+++ app.js
@@ -1,17 +1,44 @@
 const hamburgerMenu = document.querySelector('.hamburger-menu');
 const navList = document.querySelector('.nav-list');
 const navLink = document.querySelectorAll('.nav-link')
-hamburgerMenu.addEventListener('click', ()=>{
-    navList.classList.toggle('show-nav-list')
-    hamburgerMenu.classList.toggle('rotate');
-})
-navLink.forEach((link)=>{
-link.addEventListener('click',()=>{
-    navList.classList.toggle('show-nav-list')
-    hamburgerMenu.classList.toggle('rotate');
-})
-})
 
+evtsArray = ['click','touchstart','touchend']
+
+evtsArray.forEach( evt => {
+    console.log(evt)
+    if(evt == 'click'){ 
+        hamburgerMenu.addEventListener('click', ()=>{
+            navList.classList.toggle('show-nav-list')
+            hamburgerMenu.classList.toggle('rotate');
+        })   
+    }
+    if(evt == 'touchstart'){
+        hamburgerMenu.addEventListener('touchstart', ()=>{
+            hamburgerMenu.classList.add('touch-hover');
+           
+        })
+    }
+    if(evt == 'touchend'){
+        hamburgerMenu.addEventListener('touchend', ()=>{
+                hamburgerMenu.classList.remove('touch-hover');  
+            })
+    }
+} 
+);
+navLink.forEach((link)=>{
+    link.addEventListener('click',()=>{
+        navList.classList.toggle('show-nav-list')
+        hamburgerMenu.classList.toggle('rotate');
+    })
+    })
+const cta = document.querySelector('.cta');
+
+cta.addEventListener('touchstart', ()=>{
+   cta.classList.add('cta-touch-hover');
+})
+cta.addEventListener('touchend', ()=>{
+    cta.classList.remove('cta-touch-hover');
+})
 
 
     
